d47265b4719a40f5f276c89f2607218cf857bdce
Pride1922 <48358139+Pride1922@users.noreply.github.com>
2020-12-17 12:10:53 +0100

    Update Items.sql

--- Kuzkay_Coke/Items.sql
+++ Kuzkay_Coke/Items.sql
@@ -2,5 +2,7 @@ INSERT INTO `items` (`name`, `label`, `weight`, `rare`, `can_remove`) VALUES ('c
 
 INSERT INTO `items` (`name`, `label`, `weight`, `rare`, `can_remove`) VALUES ('coke_pooch', 'Bagged Cocaine', 2, 0, 1);
 
-INSERT INTO `items` (`name`, `label`, `weight`, `rare`, `can_remove`) VALUES ('coke_oil', 'Oleo de coca', 2, 0, 1);
+INSERT INTO `items` (`name`, `label`, `weight`, `rare`, `can_remove`) VALUES ('coke_oil', 'Oleo de coca', 1, 0, 1);
+
+INSERT INTO `items` (`name`, `label`, `weight`, `rare`, `can_remove`) VALUES ('soap', 'Sabão', 2, 0, 1);
 
